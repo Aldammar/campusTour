f8d5a169b45d046c984d79927db119419010c90b
StrableD <89165077+StrableD@users.noreply.github.com>
2024-11-15 18:49:52 +0100

    updated the ar files with split functions and doc
    the logic for the audio and track files changed. audios and tracks are now set in the html.
    the component changed from a component for the scene to a component per marker. therefor the logic for audio and track is per marker.

--- ar-script.js
+++ ar-script.js
@@ -1,30 +1,103 @@
+/**
+ * Function to handle window load event.
+ * Initializes a clean URL, input field listener, and decoration image placement.
+ */
 window.onload = () => {
+    getCleanUrl();
+
+    setupInputFieldListener();
+
+    setupAudioTracks();
+
+    placeDecorationImages();
+};
+
+/**
+ * A-Frame component to handle audio playback for markers.
+ */
+AFRAME.registerComponent('audiomarker', {
+    /**
+     * Initializes the component.
+     * Sets up event listeners for marker found and lost events.
+     */
+    init: function () {
+        const marker = this.el;
+        const audio = marker.querySelector("audio");
+
+        marker.addEventListener('markerFound', () => {
+            console.info(`Marker ${marker.id} found`);
+            audio.play();
+            console.info(`Playing ${audio.currentSrc}`);
+        });
+        marker.addEventListener('markerLost', () => {
+            console.info(`Marker ${marker.id} lost`);
+            audio.pause();
+            console.info(`Paused ${audio.currentSrc}`);
+        });
+    }
+})
+
+/**
+ * Function to clean and redirect the URL.
+ */
+function getCleanUrl() {
     const url_arr = document.location.href.split("/");
     if (url_arr[url_arr.length - 1] !== "campusTour") {
         url_arr.pop();
     }
     document.location.url = url_arr.join("/") + "/";
+}
 
+/**
+ * Function to set up an event listener for the input field.
+ * Listens for the 'Enter' key press to trigger the checkSentence function.
+ */
+function setupInputFieldListener() {
     const inputField = document.getElementById('endSentenceInput');
     inputField.addEventListener('keydown', async (event) => {
         if (event.key === 'Enter') {
             await checkSentence();
         }
     });
+}
 
-    function pxToVh(px) {
-        return px / document.documentElement.clientHeight * 100;
-    }
+/**
+ * Function to set up event listeners for audio tracks.
+ * Displays track information and handles play, pause, and end events.
+ */
+function setupAudioTracks() {
+    const audios = document.querySelectorAll("audio");
+    const trackDisplay = document.getElementById("trackDisplay");
+    audios.forEach(audio => {
+        const track = audio.querySelector("track");
+        audio.addEventListener("ended", () => {
+            trackDisplay.style.display = 'none';
+            trackDisplay.textContent = "";
+            console.info(`Ended ${audio.currentSrc} and cleared track display`);
+        });
+        audio.addEventListener("pause", () => {
+            trackDisplay.style.display = 'none';
+            console.info(`Paused ${audio.currentSrc} and hidden track display`);
+        });
+        audio.addEventListener("play", () => {
+            trackDisplay.style.display = 'block';
+            console.info(`Playing ${audio.currentSrc} and shown track display`);
+        });
+        track.addEventListener('cuechange', () => {
+            const cues = track.track.activeCues;
+            if (cues.length) {
+                trackDisplay.textContent = cues[0].text;
+            } else {
+                trackDisplay.textContent = "";
+            }
+        });
+    });
+}
 
-    function pxToVw(px) {
-        return px / document.documentElement.clientWidth * 100;
-    }
-
-    function valueOfUnitString(unitString) {
-        const unitStringRegex = /([0-9.]+)([a-z%]+)/i;
-        return parseFloat(unitStringRegex.exec(unitString)[1]);
-    }
-
+/**
+ * Function to place decoration images around the screen.
+ */
+function placeDecorationImages() {
     const images = Array.from(document.querySelectorAll('#decoration img'));
     const button = document.querySelector('#showPopupButton');
     const placedImages = [{
@@ -34,56 +107,108 @@ window.onload = () => {
         height: pxToVh(valueOfUnitString(window.getComputedStyle(button).height))
     }];
 
-    function isColliding(image) {
-        return placedImages.some(img => Math.abs(img.top - image.top) < img.height + image.height && Math.abs(img.left - image.left) < img.width + image.width)
-    }
-
-    const edgeCount = [Math.ceil(images.length / 4), Math.floor(images.length / 4), Math.floor(images.length / 4), Math.round(images.length / 4)]
+    const edgeCount = [
+        Math.ceil(images.length / 4),
+        Math.floor(images.length / 4),
+        Math.floor(images.length / 4),
+        Math.round(images.length / 4)
+    ]
 
     images.forEach(image => {
-        let edge, position, newImage;
-        let iterations = 0;
-        do {
-            do {
-                edge = Math.floor(Math.random() * 4);
-            } while (edgeCount[edge] === 0);
-            position = 1 + Math.random() * 98;
-            newImage = {top: 0, left: 0, width: pxToVw(image.width), height: pxToVh(image.height)};
-
-            switch (edge) {
-                case 0: // Top edge
-                    newImage.top = 1;
-                    newImage.left = position;
-                    break;
-                case 1: // Right edge
-                    newImage.top = position;
-                    newImage.left = 99 - pxToVw(image.width);
-                    break;
-                case 2: // Bottom edge
-                    newImage.top = 99 - pxToVh(image.height);
-                    newImage.left = position;
-                    break;
-                case 3: // Left edge
-                    newImage.top = position;
-                    newImage.left = 1;
-                    break;
-            }
-            iterations++;
-        } while (isColliding(newImage) && iterations < 100);
-        if (iterations === 100) {
-            image.style.display = 'none';
-            console.info(`Image ${image.src.split("/").pop()} could not be placed`);
-        } else {
-
-            edgeCount[edge]--;
-            placedImages.push(newImage);
-
-            image.style.top = newImage.top + "vh";
-            image.style.left = newImage.left + "vw";
-        }
+        placeImage(edgeCount, image, placedImages);
     });
-};
+}
 
+/**
+ * Function to place a single image at a random position along the edges of the screen.
+ * @param {Array} edgeCount - Array containing the count of images to be placed on each edge.
+ * @param {HTMLElement} image - The image element to be placed.
+ * @param {Array} placedImages - Array of already placed images with their positions.
+ */
+function placeImage(edgeCount, image, placedImages) {
+    let edge, position, newImage;
+    let iterations = 0;
+    do {
+        do {
+            edge = Math.floor(Math.random() * 4);
+        } while (edgeCount[edge] === 0);
+        position = 1 + Math.random() * 98;
+        newImage = {top: 0, left: 0, width: pxToVw(image.width), height: pxToVh(image.height)};
+
+        switch (edge) {
+            case 0: // Top edge
+                newImage.top = 1;
+                newImage.left = position;
+                break;
+            case 1: // Right edge
+                newImage.top = position;
+                newImage.left = 99 - pxToVw(image.width);
+                break;
+            case 2: // Bottom edge
+                newImage.top = 99 - pxToVh(image.height);
+                newImage.left = position;
+                break;
+            case 3: // Left edge
+                newImage.top = position;
+                newImage.left = 1;
+                break;
+        }
+        iterations++;
+    } while (isColliding(placedImages, newImage) && iterations < 100);
+    if (iterations === 100) {
+        image.style.display = 'none';
+        console.info(`Image ${image.src.split("/").pop()} could not be placed`);
+    } else {
+        edgeCount[edge]--;
+        placedImages.push(newImage);
+
+        image.style.top = newImage.top + "vh";
+        image.style.left = newImage.left + "vw";
+    }
+}
+
+/**
+ * Converts pixels to viewport height (vh).
+ * @param {number} px - The pixel value to be converted.
+ * @returns {number} - The equivalent value in viewport height (vh).
+ */
+function pxToVh(px) {
+    return px / document.documentElement.clientHeight * 100;
+}
+
+/**
+ * Converts pixels to viewport width (vw).
+ * @param {number} px - The pixel value to be converted.
+ * @returns {number} - The equivalent value in viewport width (vw).
+ */
+function pxToVw(px) {
+    return px / document.documentElement.clientWidth * 100;
+}
+
+/**
+ * Extracts the numeric value from a unit string (e.g., "10px").
+ * @param {string} unitString - The unit string to be parsed.
+ * @returns {number} - The numeric value extracted from the unit string.
+ */
+function valueOfUnitString(unitString) {
+    const unitStringRegex = /([0-9.]+)([a-z%]+)/i;
+    return parseFloat(unitStringRegex.exec(unitString)[1]);
+}
+
+/**
+ * Checks if a new image position collides with any already placed images.
+ * @param {Array} placedImages - Array of already placed images with their positions.
+ * @param {Object} image - The new image position to be checked.
+ * @returns {boolean} - True if the new image position collides with any placed images, false otherwise.
+ */
+function isColliding(placedImages, image) {
+    return placedImages.some(img => Math.abs(img.top - image.top) < img.height + image.height && Math.abs(img.left - image.left) < img.width + image.width)
+}
+
+/**
+ * Event handler to close the popup when clicking outside of it.
+ * @param {Event} event - The click event.
+ */
 function closePopupOnClickOutside(event) {
     const popup = document.getElementById('popup');
     const button = document.getElementById('showPopupButton');
@@ -93,12 +218,20 @@ function closePopupOnClickOutside(event) {
     }
 }
 
+/**
+ * Function to show the popup.
+ */
 function showPopup() {
     document.getElementById('popup').style.display = 'flex';
     document.getElementById('showPopupButton').style.display = 'none';
     document.addEventListener('click', closePopupOnClickOutside);
 }
 
+/**
+ * Function to check the entered sentence against the correct sentence.
+ * If correct, plays the final message audio and shows the final model.
+ * If incorrect, shows an alert message.
+ */
 function checkSentence() {
     const inputField = document.getElementById('endSentenceInput');
     const input = inputField.value;
@@ -106,104 +239,28 @@ function checkSentence() {
 
     if (correctSentence.test(input)) {
         closePopup();
+
         const model = document.querySelector('a-entity#final');
+        const audio = model.querySelector("audio");
 
-        const audio = new Audio('audios/final_message.mp3');
-        addTextTrack(audio, 'final_message')
-            .then(track => {
-                model.setAttribute('visible', true);
+        model.setAttribute('visible', true);
 
-                audio.play();
-                audio.onended = () => {
-                    model.setAttribute('visible', false);
-                    track.mode = "hidden";
-                }
-            });
+        audio.play().then(() => console.info(`Playing ${audio.currentSrc}`)).catch(e => console.error(e));
+        audio.onended = () => {
+            model.setAttribute('visible', false);
+        }
     } else {
         setTimeout(() => alert(`Der Satz "${input}" ist falsch. Bitte versuchen Sie es erneut.`), 500);
     }
     inputField.value = "";
 }
 
+
+/**
+ * Function to close the popup.
+ */
 function closePopup() {
     document.getElementById('popup').style.display = 'none';
     document.getElementById('showPopupButton').style.display = 'block';
     document.removeEventListener('click', closePopupOnClickOutside);
-}
-
-AFRAME.registerComponent('markerhandler', {
-    init: async () => {
-        const audios = {};
-        let markers = document.querySelectorAll("a-marker");
-        for (const marker of markers) {
-            const id = marker.getAttribute('id');
-            audios[id] = new Audio(`audios/${id}_message.mp3`);
-            const track = await addTextTrack(audios[id], id + "_message");
-            marker.addEventListener('markerFound', () => {
-                track.mode = "showing";
-                audios[id].play();
-            });
-            marker.addEventListener('markerLost', () => {
-                audios[id].pause();
-                track.mode = "hidden";
-            });
-        }
-    }
-})
-
-async function addTextTrack(audio, cueFileName) {
-    const track = audio.addTextTrack('subtitles', 'German', 'de');
-    track.mode = "showing";
-    const trackDisplay = document.querySelector('div#trackDisplay');
-    return fetch(`audios/${cueFileName}.vtt`)
-        .then(response => response.text())
-        .then(text => text.split(/\r?\n\r?\n/))
-        .then(cues => {
-            if (cues.length <= 1 || cues[0].split(" ")[0] !== "WEBVTT") {
-                throw `${cueFileName} not a WebVTT file`
-            } else {
-                return cues.slice(1)
-            }
-        })
-        .then(cues => {
-            cues.forEach(cue => {
-                const [time, text] = cue.split(/\r?\n/);
-                const [start, end] = time.split(" --> ")
-                    .map(time => timeToSeconds(time.trim()));
-                const vttCue = new VTTCue(parseFloat(start), parseFloat(end), text);
-                track.addCue(vttCue);
-            });
-            audio.addEventListener("ended", () => {
-                trackDisplay.style.display = 'none';
-                trackDisplay.textContent = "";
-            });
-            audio.addEventListener("pause", () => {
-                trackDisplay.style.display = 'none';
-            });
-            audio.addEventListener("play", () => {
-                trackDisplay.style.display = 'block';
-            });
-            track.addEventListener("cuechange", () => {
-                if (track.activeCues.length > 0) {
-                    trackDisplay.textContent = track.activeCues[0].text;
-                } else {
-                    trackDisplay.textContent = "";
-                }
-            });
-            return track;
-        })
-        .catch(error => console.error(error));
-}
-
-function timeToSeconds(time) {
-    const timeRegex = /^([0-9]{2}:)?([0-9]{2}):([0-9]{2}(\.[0-9]{1,3})?)$/i;
-    let result = timeRegex.exec(time);
-    if (result === null || result.length === 1) {
-        throw `Invalid time format: ${time}`;
-    }
-    result = result.slice(1).map(value => value === undefined ? 0.0 : parseFloat(value.replace(/[ :]/i, "")));
-    let hours = result[0];
-    let minutes = result[1];
-    let seconds = result[2];
-    return hours * 3600.0 + minutes * 60.0 + seconds;
 }
